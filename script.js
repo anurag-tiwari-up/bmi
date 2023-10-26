063ff32b609f42db3361de9a937b9f97c932257e
anurag-tiwari-up <anuragbalupur421@gmail.com>
2023-10-26 08:45:14 +0530

    first test commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function bmi() {
+    var h=document.getElementById('h').value;
+    var w=document.getElementById('w').value;
+    var bmi=w/(h/100*h/100);
+    var bmio=(bmi.toFixed(2));
+    document.getElementById("result").innerHTML="Your BMI is "+bmio;
+}
